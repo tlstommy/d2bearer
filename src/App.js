d149af7f302507a35b977d038affd78893bb4609
lulamae12 <tls.tommy11@gmail.com>
2023-01-10 16:08:49 -0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import Navbar from "./components/Navbar";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        sasas
-      </header>
+      <Navbar/>
       hi
     </div>
   );
