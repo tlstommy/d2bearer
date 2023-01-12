ebfb30280881136ff801b52af0fd1bdf96ba99b9
lulamae12 <tls.tommy11@gmail.com>
2023-01-12 01:03:04 -0500

    sizing fixed?

--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -1,8 +1,8 @@
 //textinput
 export default function TextInput() {
     return (
-        <div className="">
-            <input className="textInputBox" type="text" id="textInput" name="textInput" placeholder="Input Text Here"></input>
-        </div>
+
+        <input className="textInputBox w-1/3" type="text" id="textInput" name="textInput" placeholder="Input Text Here"></input>
+
     );
 }
