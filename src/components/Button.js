d8ec5f6a8b3257a05476b60525297fe524d9930b
lulamae12 <tls.tommy11@gmail.com>
2023-01-12 12:53:31 -0500

    commit

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,6 @@
 export default function Button() {
     return (
-    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full w-1/6">
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
         Button
     </button>
     );
--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -2,7 +2,7 @@
 export default function TextInput() {
     return (
 
-        <input className="textInputBox w-1/3" type="text" id="textInput" name="textInput" placeholder="Input Text Here"></input>
+        <input className="textInputBox " type="text" id="textInput" name="textInput" placeholder="Input Text Here"></input>
 
     );
 }
