bc1b9f7375ae7a97488765a9cf96f97e42e7ff98
lulamae12 <tls.tommy11@gmail.com>
2023-01-16 16:41:44 -0500

    got step counter working

--- src/components/Container.js
+++ src/components/Container.js
@@ -15,20 +15,30 @@ export default function Container() {
   let appCliID;
   let appCliSecret;
   let textInputComponent;
+  //counts each step in the cycle of token generation
+
+
+
+  const apiURL = "https://www.bungie.net/Platform/App/OAuth/Token/"
+
+
   appCliID = "1"
   //strings for instructions so i could practice a bit with this data type
   const instructionStrings = {
-    enterClientIDString: <p>This tool provides an easy and secure way to generate a bearer token for Bungie.net's Destiny 2 api.<br/><br/>You can access the Bungie developer portal&nbsp; 
+    enterClientIDString: <p>This tool provides an easy and secure way to generate a oAuth bearer token for Bungie.net's Destiny 2 api.<br/><br/>You can access the Bungie developer portal&nbsp; 
     <a class="hover:underline text-blue-600" href="https://www.bungie.net/en/Application">here.</a>
-    <br/><br/>To begin, please enter your Application's Client ID below then click submit.</p>,
-    enterClientSecretString: <p>Now, please enter your Application's Client Secret below then click submit.</p>,
+    <br/><br/>To begin, please enter your Application's Client ID below and click submit.</p>,
+    
+    enterClientSecretString: <p><b>App Client ID: </b>{appCliID}<br/><br/><hr/><br/>Now, please enter your Application's Client Secret below and click submit.</p>,
 
+    openAuthUrlString: <p><b>App Client ID: </b>{appCliID}<br/><b>App Client Secret: </b>{appCliSecret}<br/><br/><hr/><br/>Now, please enter your Application's Client Secret below and click submit.</p>,
   }
 
   // Declare a new state variable, inputData and set setdata to input data
   const [inputData, setData] = useState('');
   //use setTextData(""); to update textboxText
   const [textboxText, setTextData] = useState('');
+  const [stepCounter, setStepCounter] = useState(0);
 
   //use effect with an empty cond will trigger on page load.
   useEffect(() => {
@@ -41,7 +51,12 @@ export default function Container() {
     setData(e);
   }
   
-  
+  //this may be a bit of scuffed way to do this
+  function updateAppData(step,data){
+    console.log(stepCounter);
+    console.log(data);
+
+  }
 
 
   //on button submit print out the inputData var from above
@@ -50,10 +65,13 @@ export default function Container() {
 
     //set data from submit
     userInput = inputData;
-    console.log(appCliID);
+    //console.log(appCliID);
 
-    console.log(userInput);
+    //console.log(userInput);
     //alertTest(inputData);
+
+    updateAppData(stepCounter,inputData);
+    setStepCounter(stepCounter+1)
     setTextData(instructionStrings.enterClientSecretString);
 
   }
@@ -85,7 +103,7 @@ export default function Container() {
             <div class="flex flex-col space-y-5">
               <div className="flex flex-col items-center space-y-2">
                 <TextBox textboxText={textboxText} />
-                <input className="textInputBox " type="text" id="textInput" name="textInput" placeholder="Input Text Here" onChange={event=>updateInputData(event.target.value)}></input>
+                <input className="textInputBox " type="text" id="textInput" name="textInput" placeholder="Input Data Here" onChange={event=>updateInputData(event.target.value)}></input>
                 <Button buttonText={"sUBMIT"}/>
               </div>
             </div>
