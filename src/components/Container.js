106b31dee8d6226dae7155266e6fa4c1bb50e6cb
lulamae12 <tls.tommy11@gmail.com>
2023-01-16 14:40:02 -0500

    commikt

--- src/components/Container.js
+++ src/components/Container.js
@@ -6,7 +6,7 @@ import Navbar from "./Navbar";
 import TextBox from "./TextBox";
 
 import Button from "./Button";
-import { useState } from 'react';
+import { useState, useEffect  } from 'react';
 
 
 
@@ -14,18 +14,35 @@ export default function Container() {
   let userInput;
   let appCliID;
   let appCliSecret;
+  let textInputComponent;
+  appCliID = "1"
+  //strings for instructions so i could practice a bit with this data type
+  const instructionStrings = {
+    enterClientIDString: <p>This tool provides an easy and secure way to generate a bearer token for Bungie.net's Destiny 2 api.<br/><br/>You can access the Bungie developer portal&nbsp; 
+    <a class="hover:underline text-blue-600" href="https://www.bungie.net/en/Application">here.</a>
+    <br/><br/>To begin, please enter your Application's Client ID below then click submit.</p>,
+    enterClientSecretString: <p>Now, please enter your Application's Client Secret below then click submit.</p>,
 
+  }
 
   // Declare a new state variable, inputData and set setdata to input data
   const [inputData, setData] = useState('');
+  //use setTextData(""); to update textboxText
   const [textboxText, setTextData] = useState('');
 
-
+  //use effect with an empty cond will trigger on page load.
+  useEffect(() => {
+    setTextData(instructionStrings.enterClientIDString);
+  }, []);
+  
   function updateInputData(e){
     //https://beta.reactjs.org/learn
     //set the setData var from above to e
     setData(e);
   }
+  
+  
+
 
   //on button submit print out the inputData var from above
   function submit(e) {
@@ -37,7 +54,7 @@ export default function Container() {
 
     console.log(userInput);
     //alertTest(inputData);
-    setTextData("he there");
+    setTextData(instructionStrings.enterClientSecretString);
 
   }
 
@@ -51,8 +68,7 @@ export default function Container() {
   }
   
   
-  
-  
+ 
   return (
     <div>
       <Navbar/>
@@ -66,9 +82,9 @@ export default function Container() {
               </h1>
           </div>
           <form onSubmit={submit}>
-            <div class="flex flex-col items-center space-y-5">
+            <div class="flex flex-col space-y-5">
               <div className="flex flex-col items-center space-y-2">
-                <TextBox textboxText={textboxText}/>
+                <TextBox textboxText={textboxText} />
                 <input className="textInputBox " type="text" id="textInput" name="textInput" placeholder="Input Text Here" onChange={event=>updateInputData(event.target.value)}></input>
                 <Button buttonText={"sUBMIT"}/>
               </div>
--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -1,8 +1,8 @@
 //textbox to show messages
 export default function TextBox(props) {
     return (
-      <div className="textBox transition-all break-normal w-2/3">
-        <p>{props.textboxText}</p>
+      <div className="textBox break-normal w-3/4">
+        <p defaultValue={props.defaultValue}>{props.textboxText}</p>
       </div>
     );
 }
