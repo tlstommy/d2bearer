ae55f798f98b7d442c6ddd3c79f42809e828a24b
lulamae12 <tls.tommy11@gmail.com>
2023-02-13 14:02:45 -0500

    commit for now

--- src/components/Container.js
+++ src/components/Container.js
@@ -178,48 +178,46 @@ async function grabBearerToken(id,secret,code){
           </div>
           <hr/>
           <div>
-            <div class="px-4 py-5 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
+            <div class="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
               <dt class="text-sm font-medium text-black sm:col-span-1">Access Token</dt>
               <dd class="overflow-x-auto mt-1 text-sm text-white bg-slate-700 sm:col-span-2 sm:mt-0 scrollbar-hide"><code>{postResponseJSON["access_token"]}</code></dd>
-              <dd class="sm:col-span-1"><DocumentDuplicateIcon className="h-6 w-6 text-blue-500"/></dd>
             </div>
             <hr/>
-            <div class="px-4 py-5 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
+            <div class="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
               <dt class="text-sm font-medium text-black sm:col-span-1">Access Token Type</dt>
-              <dd class="mt-1 text-sm text-white bg-slate-700 sm:col-span-3 sm:mt-0"><code>{postResponseJSON["token_type"]}</code></dd>
+              <dd class="mt-1 text-sm text-white bg-slate-700 sm:col-span-2 sm:mt-0"><code>{postResponseJSON["token_type"]}</code></dd>
             </div>
             <hr/>
-            <div class="px-4 py-5 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
+            <div class="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
               <dt class="text-sm font-medium text-black sm:col-span-1">Access Token Expiry</dt>
-              <dd class="mt-1 text-sm text-white bg-slate-700 sm:col-span-3 sm:mt-0"><code>{postResponseJSON["expires_in"]} seconds. [{calculateExpiryTime(postResponseJSON["expires_in"])}]</code></dd>
+              <dd class="mt-1 text-sm text-white bg-slate-700 sm:col-span-2 sm:mt-0"><code>{postResponseJSON["expires_in"]} seconds. [{calculateExpiryTime(postResponseJSON["expires_in"])}]</code></dd>
             </div>
             <hr/>
-            <div class="px-4 py-5 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
+            <div class="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
               <dt class="text-sm font-medium text-black sm:col-span-1">Refresh Token</dt>
               <dd class="overflow-x-auto mt-1 text-sm text-white bg-slate-700 sm:col-span-2 sm:mt-0 scrollbar-hide"><code>{postResponseJSON["refresh_token"]}</code></dd>
-              <dd class=" sm:col-span-1"><DocumentDuplicateIcon className="h-6 w-6 text-blue-500"/></dd>
             </div>
             <hr/>
-            <div class="px-4 py-5 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
+            <div class="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
               <dt class="text-sm font-medium text-black sm:col-span-1">Refresh Token Expiry</dt>
-              <dd class="mt-1 text-sm text-white bg-slate-700 sm:col-span-3 sm:mt-0"><code>{postResponseJSON["refresh_expires_in"]} seconds. [{calculateExpiryTime(postResponseJSON["refresh_expires_in"])}]</code></dd>
+              <dd class="mt-1 text-sm text-white bg-slate-700 sm:col-span-2 sm:mt-0"><code>{postResponseJSON["refresh_expires_in"]} seconds. [{calculateExpiryTime(postResponseJSON["refresh_expires_in"])}]</code></dd>
             </div>
             <hr/>
-            <div class="px-4 py-5 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
+            <div class="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
               <dt class="text-sm font-medium text-black sm:col-span-1">App Membership ID</dt>
-              <dd class="mt-1 text-sm text-white bg-slate-700 sm:col-span-3 sm:mt-0"><code>{postResponseJSON["membership_id"]}</code></dd>
+              <dd class="mt-1 text-sm text-white bg-slate-700 sm:col-span-2 sm:mt-0"><code>{postResponseJSON["membership_id"]}</code></dd>
             </div>
             <hr/>
             <div class="px-4 py-5 sm:grid sm:grid-cols-1 sm:gap-4 sm:px-6">
-              <div id="accordion-collapse" data-accordion="collapse">
+              <div id="accordionExample" data-accordion="collapse">
                 <h2 id="accordion-collapse-heading-1">
                   <button type="button" class="flex items-center justify-between w-full" data-accordion-target="#accordion-collapse-body-1" aria-expanded="true" aria-controls="accordion-collapse-body-1">
                     <span>Full JSON Response</span>
                     <svg data-accordion-icon class="w-6 h-6 rotate-180 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                   </button>
                 </h2>
-                <div id="accordion-collapse-body-1" class="hidden" aria-labelledby="accordion-collapse-heading-1">
-                  <div class="p-5">
+                <div id="accordion-collapse-body-1" class="accordion-collapse collapse" aria-labelledby="accordion-collapse-heading-1" data-bs-parent="#accordionExample">
+                  <div class="accordion-body p-5">
                     <p class="mb-2 text-gray-500 dark:text-gray-400">Flowbite is an open-source library of interactive components built on top of Tailwind CSS including buttons, dropdowns, modals, navbars, and more.</p>
                     <p class="text-gray-500 dark:text-gray-400">Check out this guide to learn how to <a href="/docs/getting-started/introduction/" class="text-blue-600 dark:text-blue-500 hover:underline">get started</a> and start developing websites even faster with components on top of Tailwind CSS.</p>
                   </div>
