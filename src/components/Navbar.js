71f9e4c63ddd6f9503752399736a1a070a2617dc
lulamae12 <tls.tommy11@gmail.com>
2023-04-24 14:00:01 -0400

    commit for pages test

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 export default function Navbar() {
     return (
-      <header class="mb-2 mt-0 text-4xl bg-gray-500 py-5 sfont-medium leading-tight text-primary">
-        
+      <header class="mb-2 mt-0 text-2xl bg-gray-500 py-5 px-3 sfont-medium leading-tight text-primary">
+        <b>D2-Bearer</b>
       </header>
     );
   }
