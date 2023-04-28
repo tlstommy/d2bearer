47536ae851f102c80d24b91c3917661e05c48fbc
lulamae12 <tls.tommy11@gmail.com>
2023-04-28 09:35:57 -0400

    COMMIT NAV STUFF

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,15 @@
 export default function Navbar() {
     return (
       <header>
-        <nav class="mb-2 mt-0 text-2xl bg-gray-500 py-5 px-3 sfont-medium leading-tight text-primary">
-          <b>D2-Bearer</b>
+        <nav class="mb-2 mt-0 text-2xl bg-gray-500 py-5 px-3 lg:px-6 sfont-medium leading-tight text-primary">
+          <div class="flex flex-wrap justify-between items-center">
+            <div class="flex justify-start items-center">
+              <b>D2-Bearer</b>
+            </div>
+            <div class="flex items-center lg:order-2">
+                <b>[GH]</b>
+            </div>
+          </div>
         </nav>
       </header>
     );
